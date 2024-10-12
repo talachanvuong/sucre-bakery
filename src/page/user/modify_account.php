4f6da74612e1e55368535a363293bbf4366646b9
Highland <165888373+Highland04@users.noreply.github.com>
2024-10-12 18:02:07 +0700

    -Update login&register -Add modify_account for user

--- src/page/user/modify_account.php
+++ src/page/user/modify_account.php
@@ -1,14 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="../../css/user/login.css">
+    <title>Sửa thông tin </title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="../../css/user/modify_account.css">
 </head>
-
 <body>
     <header>
         <a href="#" class="logo">sucré bakery</a>
@@ -22,24 +20,25 @@
         </nav>
         <button id="menu-dropdown" class="menu-dropdown">&#8801</button>
     </header>
-    <div class="login-container">
-        <h2>Đăng Nhập</h2>
-        <form action="login.php" method="post">
+    <div class="modify-container">
+        <h2>Sửa thông tin</h2>
+        <form action="modify.php" method="post">
+
+            <div class="input-group">
+                <label for="name">Họ và tên:</label>
+                <input type="name" id="name" name="name" placeholder="Nhập họ và tên cảu bạn" required>
+            </div>
+            <div class="input-group">
+                <label for="address">Địa chỉ:</label>
+                <input type="text" id="address" name="address" placeholder="Nhập địa chỉ của bạn" required>
+            </div>
             <div class="input-group">
                 <label for="phone">Số điện thoại:</label>
                 <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" maxlength="10" placeholder="Nhập số điện thoại của bạn" required>
             </div>
-            <div class="input-group">
-                <label for="password">Mật khẩu:</label>
-                <input type="password" id="password" name="password" placeholder="Nhập mật khẩu" required>
-            </div>
-            <button type="submit" class="btn">Đăng nhập</button>
-            <div class="register">
-                <p>Chưa có tài khoản? <a href="register.php">Đăng ký ngay.</a></p>
-            </div>
+            <button type="submit" class="btn">Cập nhật</button>
         </form>
     </div>
     <script src="../../js/user/form.js"></script>
 </body>
-
 </html>
