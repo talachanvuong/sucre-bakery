42a81db32ad03175500172dd925344d496f6d5a6
Nguyen Hau <165163551+trunghau05@users.noreply.github.com>
2024-10-27 11:46:52 +0700

    admin/product

--- src/page/admin/product.php
+++ src/page/admin/product.php
@@ -6,8 +6,9 @@ if ($connection->connect_error) {
     die("Kết nối thất bại: " . $connection->connect_error);
 }
 
-function get_products($connection) {
-    $sql = "SELECT * FROM `product`";
+function get_products($connection)
+{
+    $sql = "SELECT * FROM product";
     $result = $connection->query($sql);
     return $result->fetch_all(MYSQLI_ASSOC);
 }
@@ -39,11 +40,11 @@ if (isset($_POST['updateProduct'])) {
     $type = $_POST['productType'];
     $image = $_FILES['productImage']['tmp_name'] ? "0x" . bin2hex(file_get_contents($_FILES['productImage']['tmp_name'])) : null;
 
-    $sql = "UPDATE `product` SET `pd_name` = ?, `pd_price` = ?, `pd_description` = ?, `pdt_id` = ?";
+    $sql = "UPDATE product SET pd_name = ?, pd_price = ?, pd_description = ?, pdt_id = ?";
     if ($image) {
-        $sql .= ", `pd_image` = ?";
+        $sql .= ", pd_image = ?";
     }
-    $sql .= " WHERE `pd_id` = ?";
+    $sql .= " WHERE pd_id = ?";
 
     $stmt = $connection->prepare($sql);
 
@@ -64,7 +65,7 @@ if (isset($_POST['updateProduct'])) {
 
 if (isset($_POST['deleteProduct'])) {
     $id = $_POST['productId'];
-    $stmt = $connection->prepare("DELETE FROM `product` WHERE `pd_id` = ?");
+    $stmt = $connection->prepare("DELETE FROM product WHERE pd_id = ?");
     $stmt->bind_param("i", $id);
 
     if ($stmt->execute()) {
@@ -73,6 +74,16 @@ if (isset($_POST['deleteProduct'])) {
         $message = "Lỗi: " . $stmt->error;
     }
 
+    $stmt->close();
+}
+
+if (isset($_POST['editProduct'])) {
+    $id = $_POST['productId'];
+    $sql = "SELECT * FROM product WHERE pd_id = ?";
+    $stmt = $connection->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $product = $stmt->get_result()->fetch_assoc();
     $stmt->close();
 }
 
@@ -86,33 +97,158 @@ $products = get_products($connection);
     <link rel="stylesheet" href="../../css/admin/overview/menu-left.css">
     <link rel="stylesheet" href="../../css/admin/product/product.css">
     <style>
-        body { background-color: rgb(223, 223, 223); }
-        .container { display: flex; }
-        .menu-left { width: 250px; background-color: white; padding: 10px; }
-        .content { margin-left: 20px; }
-        table { width: 100%; border-collapse: collapse; }
-        th, td { border: 1px solid #000; padding: 8px; text-align: center; }
+        body {
+            background-color: rgb(223, 223, 223);
+        }
+
+        .container {
+            display: grid;
+            grid-template-areas: 
+            "Menu Edit Content"
+            "Menu Edit Content"
+            "Menu Edit Content";
+            grid-template-columns: 1fr 1fr 4fr;
+        }
+
+        .content {
+            grid-area: Content;
+            margin-left: 20px;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            border: 1px solid #000;
+            padding: 8px;
+            text-align: center;
+        }
+
+        .menu-left {
+            grid-area: Menu;
+            display: flex;
+            flex-direction: column;
+            background-color: white;
+            height: auto;
+            width: 250px;
+        }
+
+        #label {
+            background-color: aqua;
+            margin: 0px;
+            padding: 5px 0px 5px 0px;
+        }
+
+        #input {
+            margin: 5px 20px 5px 20px;
+            padding: 5px 0px 5px 0px;
+            height: 20px;
+            width: 200px;
+
+        }
+
+        .over-view p {
+            background-color: aqua;
+            margin: 0px;
+            padding: 5px 0px 5px 0px;
+        }
+
+        .over-view ul {
+            display: block;
+            padding: 0px;
+            margin: 0px;
+            text-align: center;
+        }
+
+        #list-items li {
+            padding: 5px;
+        }
+
+        #list-items li:hover {
+            background-color: rgb(220, 229, 237);
+        }
+
+        .product {
+            padding: 8px 0 8px 0;
+        }
+
+        .product p {
+            background-color: aqua;
+            margin: 0px;
+            padding: 5px 0px 5px 0px;
+        }
+
+        .product ul {
+            display: block;
+            padding: 0px;
+            margin: 0px;
+            text-align: center;
+        }
+
+        #product-items li {
+            padding: 5px;
+        }
+
+        #product-items li:hover {
+            background-color: rgb(220, 229, 237);
+        }
+
+        .edit-product {
+            grid-area: Edit;
+        }
+
+        a {
+            text-decoration: none;
+            color: black;
+        }
+
+        h1 {
+            background-color: red;
+            font-size: larger;
+            text-align: center;
+        }
     </style>
 </head>
 
 <body>
     <div class="container">
         <div class="menu-left">
-            <h3>Quản lý Sản phẩm</h3>
-            <form method="POST" enctype="multipart/form-data">
-                <input type="hidden" name="productId" value="<?= htmlspecialchars($_POST['productId'] ?? '') ?>">
-                <div><label>Tên sản phẩm:</label><input type="text" name="productName" required></div>
-                <div><label>Giá:</label><input type="number" name="productPrice" required></div>
-                <div><label>Mô tả:</label><textarea name="productDescription" required></textarea></div>
-                <div><label>Loại:</label><input type="text" name="productType" required></div>
-                <div><label>Hình ảnh:</label><input type="file" name="productImage" accept="image/*"></div>
-                <button type="submit" name="addProduct">Thêm</button>
-                <button type="submit" name="updateProduct">Sửa</button>
-            </form>
+            <div class="menu-left">
+                <div class="search-input">
+                    <p id="label">Tìm kiếm</p>
+                    <input id="input" type="text" placeholder="Theo tên sản phẩm...">
+                </div>
+                <div class="over-view">
+                    <p id="view-list"><a href="?direct=home">Thống kê tổng quan</a></p>
+                </div>
+                <div class="product">
+                    <p id="product-list">Sản phẩm</p>
+                    <ul style="list-style: none;" id="product-items">
+                        <li><a href="#">Bánh ngọt</a></li>
+                        <li><a href="#">Bánh bông lan</a></li>
+                        <li><a href="#">Bánh trung thu</a></li>
+                        <li><a href="#">Bánh kem</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
+        <form method="POST" enctype="multipart/form-data" class="edit-product">
+            <h1>Thao tác sản phẩm/h1>
+            <input type="hidden" name="productId" value="<?= htmlspecialchars($product['pd_id'] ?? '') ?>">
+            <div><label>Tên sản phẩm:</label><input type="text" name="productName" value="<?= htmlspecialchars($product['pd_name'] ?? '') ?>" required></div>
+            <div><label>Giá:</label><input type="number" name="productPrice" value="<?= htmlspecialchars($product['pd_price'] ?? '') ?>" required></div>
+            <div><label>Mô tả:</label><textarea name="productDescription" required><?= htmlspecialchars($product['pd_description'] ?? '') ?></textarea></div>
+            <div><label>Loại:</label><input type="text" name="productType" value="<?= htmlspecialchars($product['pdt_id'] ?? '') ?>" required></div>
+            <div><label>Hình ảnh:</label><input type="file" name="productImage" accept="image/*"></div>
+            <button type="submit" name="addProduct">Thêm</button>
+            <button type="submit" name="updateProduct">Sửa</button>
+        </form>
 
         <div class="content">
-            <h3>Danh sách sản phẩm</h3>
+            <h1>Danh sách sản phẩm</h1>
             <table>
                 <thead>
                     <tr>
@@ -135,7 +271,11 @@ $products = get_products($connection);
                                 <img src="data:image/jpeg;base64,<?= base64_encode(hex2bin(substr($product['pd_image'], 2))) ?>" width="50">
                             </td>
                             <td>
-                                <form method="POST">
+                                <form method="POST" style="display:inline;">
+                                    <input type="hidden" name="productId" value="<?= $product['pd_id'] ?>">
+                                    <button type="submit" name="editProduct">Sửa</button>
+                                </form>
+                                <form method="POST" style="display:inline;">
                                     <input type="hidden" name="productId" value="<?= $product['pd_id'] ?>">
                                     <button type="submit" name="deleteProduct">Xóa</button>
                                 </form>
