55f3d8006da2be94f43a1ba5f009437446b6eb9e
Highland <165888373+Highland04@users.noreply.github.com>
2024-11-14 15:09:03 +0700

    xong mấy cái của footer + đồng bộ vài cái file css

--- src/user/page/footer/term.php
+++ src/user/page/footer/term.php
@@ -1,56 +1,33 @@
-<head>
-    <link rel="stylesheet" href="../../../css/user/footer/term.css">
-</head>
-<div class="term-contener">
-    <h1>Điều Khoản </h1>
-    <p>
-        Điều khoản và điều kiện<br>
-        Cập nhật lần cuối: 22 tháng 9 năm 2024<br>
-        Điều 1: Tài khoản<br>
-        1. Bạn phải ít nhất 17 tuổi để sử dụng dịch vụ của chúng tôi.<br>
-        2. Bạn có trách nhiệm duy trì tính bảo mật của tài khoản.<br>
-        3. Bạn hoàn toàn chịu trách nhiệm cho mọi hoạt động diễn ra trong tài khoản và mọi hành động khác được thực hiện
-        liên quan đến tài khoản đó.<br>
-        4. Chúng tôi có thể theo dõi và xem xét tài khoản trước khi bạn có thể đăng nhập và bắt đầu sử dụng dịch vụ.<br>
-        5. Việc cung cấp thông tin liên hệ sai lệch dưới bất kỳ hình thức nào có thể dẫn đến việc chấm dứt tài khoản của
-        bạn.<br>
-        6. Bạn phải thông báo ngay cho chúng tôi về bất kỳ hành vi sử dụng trái phép nào đối với tài khoản của bạn hoặc
-        bất kỳ vi phạm bảo mật nào khác.<br>
-        7. Chúng tôi sẽ không chịu trách nhiệm đối với bất kỳ hành vi hoặc thiếu sót nào của bạn, bao gồm bất kỳ thiệt
-        hại nào phát sinh do các hành vi hoặc thiếu sót đó.<br>
-        8. Chúng tôi có thể xóa tài khoản của bạn nếu chúng tôi xác định rằng bạn đã vi phạm bất kỳ điều khoản nào của
-        thỏa thuận này hoặc hành vi hoặc nội dung của bạn có xu hướng làm tổn hại đến danh tiếng và danh dự của chúng
-        tôi.<br>
-    </p>
-    <p>
-        Điều 2: Liên kết đến các trang web khác<br>
-        1. Dịch vụ của chúng tôi có thể chứa các liên kết đến các trang web khác không do chúng tôi điều hành.<br>
-        2. Chúng tôi không chịu trách nhiệm kiểm tra hoặc đánh giá và chúng tôi không bảo đảm các dịch vụ của bất kỳ
-        doanh nghiệp hoặc cá nhân nào hoặc nội dung của các tài nguyên của họ.<br>
-        3. Chúng tôi không chịu bất kỳ trách nhiệm hoặc nghĩa vụ nào đối với các hành động, sản phẩm, dịch vụ và nội
-        dung của bất kỳ bên thứ ba nào khác.<br>
-        4. Bạn nên xem xét kỹ lưỡng các điều khoản và điều kiện khác của bất kỳ tài nguyên nào mà bạn truy cập thông qua
-        liên kết trên trang web của chúng tôi.<br>
-        5. Việc bạn liên kết đến bất kỳ tài nguyên khác ngoài dịch vụ của chúng tôi là do bạn tự chịu rủi ro.<br>
-    </p>
-    <p>
-        Điều 3: Thay đổi và sửa đổi<br>
-        1. Chúng tôi có quyền sửa đổi điều khoản và điều kiện này bất cứ lúc nào.<br>
-        2. Chúng tôi sẽ cập nhật ngày sửa đổi ở đầu trang này.<br>
-        3. Chúng tôi khuyến nghị bạn nên xem lại điều khoản và điều kiện này thường xuyên để nắm được các thay đổi
-        mới.<br>
-        4. Phiên bản cập nhật này sẽ có hiệu lực khi được đăng tải trên trang này.<br>
-    </p>
-    <p>
-        Điều 4: Chấp nhận điều khoản và điều kiện<br>
-        Bạn thừa nhận rằng bạn đã đọc thoả thuận này và đồng ý với tất cả các điều khoản và điều kiện của nó. Bằng cách
-        truy cập và sử dụng trang web này, bạn đồng ý bị ràng buộc bởi thoả thuận này. Nếu bạn không đồng ý tuân thủ các
-        điều khoản của thoả thuận này, xin hãy dừng sử dụng dịch vụ của chúng tôi.<br>
-    </p>
-    <p>
-        Điều 5: Thông tin liên hệ<br>
-        Nếu bạn có bất kỳ thắc mắc gì về chính sách bảo mật này, bạn có thể liên hệ với chúng tôi bằng cách:<br>
-        1. Nhắn tin trực tiếp tại fanpage: VAir Zone.<br>
-        2. Gửi thư tới địa chỉ email: vair.zone@gmail.com.<br>
-    </p>
+<div class="layout-container">
+    <div class= " term-container">
+        <p class="term-title">Điều Khoản </p>
+            <div class="term-content">
+            <h3>1. Chính Sách Bảo Mật Thông Tin Cá Nhân</h3>
+                <p><strong>Mục đích thu thập thông tin:</strong> Tiệm thu thập thông tin cá nhân của khách hàng để phục vụ việc đặt hàng, thanh toán và hỗ trợ khách hàng.</p>
+                <p><strong>Cam kết bảo mật:</strong> Sucré Bakery cam kết không chia sẻ thông tin của khách hàng cho bên thứ ba nếu không có sự đồng ý.</p>
+                <p><strong>Quyền lợi của khách hàng:</strong> Khách có quyền yêu cầu xem lại, chỉnh sửa hoặc xóa thông tin của mình trong hệ thống của tiệm.</p>
+            <h3>2. Điều Khoản Sử Dụng Trang Web</h3>
+                <p><strong>Truy cập và sử dụng:</strong> Khi khách truy cập trang web, họ đồng ý tuân theo các điều khoản này.</p>
+                <p><strong>Nội dung và bản quyền:</strong> Tất cả hình ảnh, văn bản, và nội dung trên trang web đều thuộc quyền sở hữu của Sucré Bakery và không được sao chép, sử dụng lại khi không có sự đồng ý.</p>            
+            <h3>3. Điều Khoản Đặt Hàng</h3>
+                <p><strong>Yêu cầu đặt hàng:</strong> Mô tả cách khách có thể đặt hàng, thời gian đặt hàng trước tối thiểu là bao nhiêu (24 - 48 giờ cho các loại bánh đặc biệt), và điều kiện để đơn hàng được xác nhận.</p>
+                <p><strong>Hủy và thay đổi đơn hàng:</strong> Thông tin về chính sách hủy hoặc thay đổi đơn hàng cần thông báo trước bao nhiêu giờ, thay đổi về thiết kế bánh có thể mất thêm thời gian và chi phí.</p>
+        
+            <h3>4. Điều Khoản Thanh Toán</h3>
+                <p><strong>Phương Thức Thanh Toán:</strong> Khách hàng có thể thanh toán bằng tiền mặt, chuyển khoản hoặc các phương thức điện tử như Momo, ZaloPay...</p>
+                <p><strong>Quy định thanh toán trước:</strong> Đối với đơn hàng lớn hoặc bánh thiết kế đặc biệt, khách cần thanh toán trước 50% giá trị đơn hàng. Số tiền còn lại sẽ thanh toán khi nhận hàng.</p>
+
+            <h3>5. Điều Khoản Giao Hàng</h3>
+                <p><strong>Phạm Vi Giao Hàng:</strong> Phí giao hàng dựa trên khoảng cách và khối lượng đơn hàng.</p>
+                <p><strong>Thời Gian Giao Hàng:</strong> Sucré Bakery sẽ giao hàng theo thời gian đã thỏa thuận, nhưng có thể linh động trong trường hợp phát sinh sự cố (giao thông, thời tiết).</p>
+                <p><strong>Nhận Hàng Tại Cửa Hàng:</strong> Sucré Bakery sẽ chịu trách nhiệm về chất lượng sản phẩm khi đến tay khách hàng. Khách hàng có trách nhiệm kiểm tra ngay khi nhận hàng.</p>
+        
+            <h3>6. Chính Sách Đổi Trả</h3>
+                <p><strong>Trường Hợp Đổi Trả:</strong> Chỉ áp dụng cho các trường hợp bánh bị lỗi từ phía tiệm (sai kích thước, màu sắc, mẫu mã so với đơn đặt hàng).</p>
+                <p><strong>Điều Kiện Đổi Trả:</strong> Bánh phải được giữ nguyên trạng và khách hàng cần báo ngay trong vòng 24 giờ từ khi nhận bánh để tiệm hỗ trợ</p>
+            <h3>7. Điều Khoản Trách Nhiệm Pháp Lý</h3>
+                <p><strong>Giới hạn trách nhiệm:</strong> Sucré Bakery không chịu trách nhiệm về bất kỳ thiệt hại nào do lỗi truy cập trang web, gián đoạn dịch vụ, hoặc các lỗi kỹ thuật khác không thuộc quyền kiểm soát.</p>
+                <p><strong>Thay đổi điều khoản:</strong> Sucré Bakery có quyền thay đổi, cập nhật điều khoản mà không cần báo trước. Những thay đổi sẽ được đăng tải trên trang web và có hiệu lực ngay khi công bố.</p>
+            </div>
+    </div>
 </div>
